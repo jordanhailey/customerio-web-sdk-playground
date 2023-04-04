ae20367a59ef3510474f0129933f76da79716e5f
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-04-04 16:01:28 -0700

    adding collection, nav, and helper filter

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,17 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.setServerOptions({
     watch: ['_site/**/*.css'],
   });
+
+  // Filters
+  eleventyConfig.addFilter("pretty_json",(j)=>(JSON.stringify(j,null,2)))
+
+  // Collections
+  eleventyConfig.addCollection("pages", function(collectionApi) {
+    // get all pages
+    return collectionApi.getAll().map(({data:{permalink,title,navIndex,link_title}})=>{
+      return {permalink,title,navIndex,link_title}
+    }).sort((a,b) => (a.navIndex > b.navIndex) ? 1 : ((b.navIndex > a.navIndex) ? -1 : 0))
+  });
   return {
     dir: {
       input: "src",
---
+++ README.md
@@ -0,0 +1,12 @@
+# Customer.io JavaScript SDK playground
+
+## Setup Instructions
+1. Clone this repo and `cd` (change directory) into that cloned directory
+2. Install dependencies
+    ```sh
+    $ npm i # OR yarn OR pnpm i
+    ```
+3. Start
+    ```sh
+    $ npm start # OR yarn start OR pnpm run start
+    ```
