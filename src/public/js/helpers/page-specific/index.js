428c11ddc2fa0a29de359c517d1631961f2deb13
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-04-05 17:51:50 -0700

    toning down the delay

--- src/public/js/helpers/page-specific/index.js
+++ src/public/js/helpers/page-specific/index.js
@@ -1,18 +1,23 @@
 import { getIdentifier, getAnonymousID } from "../cio-helpers.js";
 
-let identifier = getIdentifier();
-let anonymousIdentifier = getAnonymousID();
-document.getElementById(
-  "show-url"
-).innerHTML = `<em class="text-purple-500">${window.location.href}</em>`;
-if (identifier) {
-  document.getElementById(
-    "show-identifier"
-  ).innerHTML = `<div class="p-2">Your Identifier is: <em class="text-purple-500">${identifier}</em></div>`;
-} else if (anonymousIdentifier) {
-  document.getElementById(
-    "show-identifier"
-  ).innerHTML = `<div class="p-2">Your Anonymous ID: <em class="text-purple-500">${anonymousIdentifier}</em></div>`;
-} else {
-  console.warn("Couldn't find identifier", {identifier,anonymousIdentifier})
-}
+document.getElementById("show-url").innerHTML = `<em class="text-purple-500">${window.location.href}</em>`;
+const showIdentifier = document.getElementById("show-identifier");
+let intervalAttempt = 0;
+
+let cioLoadedInterval = setInterval(()=>{
+  intervalAttempt++
+  let identifier = getIdentifier();
+  let anonymousIdentifier = getAnonymousID();
+  if (identifier) {
+    showIdentifier.innerHTML = `<div class="p-2">Your Identifier is: <em class="text-purple-500">${identifier}</em></div>`;
+    clearInterval(cioLoadedInterval)
+  } else if (anonymousIdentifier) {
+    showIdentifier.innerHTML = `<div class="p-2">Your Anonymous ID: <em class="text-purple-500">${anonymousIdentifier}</em></div>`;
+    clearInterval(cioLoadedInterval)
+  }
+  // If not found, continue attempting every ~10ms for about 1 second
+  if (intervalAttempt > 100) {
+    console.warn("no identifier found")
+    clearInterval(cioLoadedInterval)
+  }
+},10)
