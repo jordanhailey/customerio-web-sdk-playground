6da05db99dadfc5eae23283efef15e5a9f664226
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-04-05 22:51:19 -0700

    update index page

--- src/public/js/helpers/page-specific/index.js
+++ src/public/js/helpers/page-specific/index.js
@@ -1,23 +1,7 @@
-import { getIdentifier, getAnonymousID } from "../cio-helpers.js";
+import { getIdentifier, getAnonymousID, showIdentifierElements } from "../cio-helpers.js";
 
-document.getElementById("show-url").innerHTML = `<em class="text-purple-500">${window.location.href}</em>`;
+document.getElementById("show-url").innerHTML = `<em class="text-purple-500 font-bold">${window.location.href}</em>`;
 const showIdentifier = document.getElementById("show-identifier");
 let intervalAttempt = 0;
 
-let cioLoadedInterval = setInterval(()=>{
-  intervalAttempt++
-  let identifier = getIdentifier();
-  let anonymousIdentifier = getAnonymousID();
-  if (identifier) {
-    showIdentifier.innerHTML = `<div class="p-2">Your Identifier is: <em class="text-purple-500">${identifier}</em></div>`;
-    clearInterval(cioLoadedInterval)
-  } else if (anonymousIdentifier) {
-    showIdentifier.innerHTML = `<div class="p-2">Your Anonymous ID: <em class="text-purple-500">${anonymousIdentifier}</em></div>`;
-    clearInterval(cioLoadedInterval)
-  }
-  // If not found, continue attempting every ~10ms for about 1 second
-  if (intervalAttempt > 100) {
-    console.warn("no identifier found")
-    clearInterval(cioLoadedInterval)
-  }
-},10)
+showIdentifierElements()
