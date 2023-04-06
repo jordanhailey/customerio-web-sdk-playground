e5c1247ba5e03333b0823263c718a80e24fae6e6
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-04-05 17:29:19 -0700

    trying without awaiting window 'load' event

--- src/public/js/helpers/page-specific/index.js
+++ src/public/js/helpers/page-specific/index.js
@@ -1,22 +1,20 @@
 import { getIdentifier, getAnonymousID } from "../cio-helpers.js";
 
-window.addEventListener("load", () => {
-  setTimeout(()=>{
-    let identifier = getIdentifier();
-    let anonymousIdentifier = getAnonymousID();
+setTimeout(()=>{
+  let identifier = getIdentifier();
+  let anonymousIdentifier = getAnonymousID();
+  document.getElementById(
+    "show-url"
+  ).innerHTML = `<em class="text-purple-500">${window.location.href}</em>`;
+  if (identifier) {
     document.getElementById(
-      "show-url"
-    ).innerHTML = `<em class="text-purple-500">${window.location.href}</em>`;
-    if (identifier) {
-      document.getElementById(
-        "show-identifier"
-      ).innerHTML = `<div class="p-2">Your Identifier is: <em class="text-purple-500">${identifier}</em></div>`;
-    } else if (anonymousIdentifier) {
-      document.getElementById(
-        "show-identifier"
-      ).innerHTML = `<div class="p-2">Your Anonymous ID: <em class="text-purple-500">${anonymousIdentifier}</em></div>`;
-    } else {
-      console.warn("Couldn't find identifier", {identifier,anonymousIdentifier})
-    }
-  },500)
-});
+      "show-identifier"
+    ).innerHTML = `<div class="p-2">Your Identifier is: <em class="text-purple-500">${identifier}</em></div>`;
+  } else if (anonymousIdentifier) {
+    document.getElementById(
+      "show-identifier"
+    ).innerHTML = `<div class="p-2">Your Anonymous ID: <em class="text-purple-500">${anonymousIdentifier}</em></div>`;
+  } else {
+    console.warn("Couldn't find identifier", {identifier,anonymousIdentifier})
+  }
+},500)
