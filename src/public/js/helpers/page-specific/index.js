377a13ce8d4836d081285c5ef6a22070a880ccf9
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-04-05 17:30:52 -0700

    testing on vercel is succesful, cutting out setTimeout

--- src/public/js/helpers/page-specific/index.js
+++ src/public/js/helpers/page-specific/index.js
@@ -1,20 +1,18 @@
 import { getIdentifier, getAnonymousID } from "../cio-helpers.js";
 
-setTimeout(()=>{
-  let identifier = getIdentifier();
-  let anonymousIdentifier = getAnonymousID();
+let identifier = getIdentifier();
+let anonymousIdentifier = getAnonymousID();
+document.getElementById(
+  "show-url"
+).innerHTML = `<em class="text-purple-500">${window.location.href}</em>`;
+if (identifier) {
   document.getElementById(
-    "show-url"
-  ).innerHTML = `<em class="text-purple-500">${window.location.href}</em>`;
-  if (identifier) {
-    document.getElementById(
-      "show-identifier"
-    ).innerHTML = `<div class="p-2">Your Identifier is: <em class="text-purple-500">${identifier}</em></div>`;
-  } else if (anonymousIdentifier) {
-    document.getElementById(
-      "show-identifier"
-    ).innerHTML = `<div class="p-2">Your Anonymous ID: <em class="text-purple-500">${anonymousIdentifier}</em></div>`;
-  } else {
-    console.warn("Couldn't find identifier", {identifier,anonymousIdentifier})
-  }
-},500)
+    "show-identifier"
+  ).innerHTML = `<div class="p-2">Your Identifier is: <em class="text-purple-500">${identifier}</em></div>`;
+} else if (anonymousIdentifier) {
+  document.getElementById(
+    "show-identifier"
+  ).innerHTML = `<div class="p-2">Your Anonymous ID: <em class="text-purple-500">${anonymousIdentifier}</em></div>`;
+} else {
+  console.warn("Couldn't find identifier", {identifier,anonymousIdentifier})
+}
