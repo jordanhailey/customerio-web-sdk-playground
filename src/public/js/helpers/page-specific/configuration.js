b94c003054f5a735bb296b1233ff4613171364b4
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-04-13 09:02:23 -0700

    config page copy changes

--- src/public/js/helpers/page-specific/configuration.js
+++ src/public/js/helpers/page-specific/configuration.js
@@ -1,6 +1,6 @@
 export default function (){
   let showSnippet = document.querySelector(".show-snippet");
-  document.querySelector(".show-snippet-text").innerText = "Your current JS snippet configuration";
-  showSnippet.innerText = `${document.getElementById("cio-tracker").outerHTML}`.replace('async=""','async').replaceAll(" ","\n  ").replace(">\x3C",">\n<");
+  document.querySelector(".show-snippet-text").innerText = "Current JS snippet configuration";
+  showSnippet.innerText = `${document.getElementById("cio-tracker").outerHTML}`.replace('async=""','async').replaceAll(" ","\n  ").replace(">","\n  >");
   showSnippet.classList.add("p-8");
 }
