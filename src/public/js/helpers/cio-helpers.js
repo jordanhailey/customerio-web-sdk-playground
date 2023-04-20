b59caabf4d66257547120de0e82a1b42e7d98441
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-04-19 23:17:11 -0700

    Revert "cdp-wip (#8)"
    This reverts commit 9f3e4050eab951121a58dfa174f3f5d00f5d7545.

--- src/public/js/helpers/cio-helpers.js
+++ src/public/js/helpers/cio-helpers.js
@@ -1,78 +1,47 @@
-import { cdpGetIdentifier } from "./cdp-helpers.js";
-import { getCurrentEpochTimestampInSeconds } from "./index.js" 
+const CIO_LOCALSTORAGE_NAMESPACE = "TSE_CIO_SDK_CONFIG";
 
-export const CIO_JS_SDK_LOCALSTORAGE_NAMESPACE = "CIO_WEB_SDK_CONFIG";
-
-const CIO_JS_SDK_DEFAULTS = {
-  siteID: "",
-  region: "regionUS",
-  autoPageTracking: "true",
+const CIO_DEFAULTS = {
+  siteID: "YOUR_SITE_ID",
+  region: "us",
 };
 
-
-
-
-/**
- * Fetches CIO Web SDK Config from localStorage, or provides defaults
- * @returns {object} `cioConfig` The configuration for the CIO Web SDK containing the following properties
- * @returns {string} `cioConfig.siteID`
- * @returns {string} `cioConfig.region`
- * @returns {string} `cioConfig.autoPageTracking`
- */
-export function cioGetConfig() {
+export function getCioConfig() {
   let siteID,
     region,
-    autoPageTracking,
     storedState = JSON.parse(
-      window.localStorage.getItem(CIO_JS_SDK_LOCALSTORAGE_NAMESPACE)
+      window.localStorage.getItem(CIO_LOCALSTORAGE_NAMESPACE)
     );
-  siteID = storedState?.siteID || CIO_JS_SDK_DEFAULTS.siteID;
-  region = storedState?.region || CIO_JS_SDK_DEFAULTS.region;
-  autoPageTracking = storedState?.autoPageTracking || CIO_JS_SDK_DEFAULTS.autoPageTracking;
-  
-  let lastUpdatedEpoch = storedState?.lastUpdated || 0;
-
-  if ((getCurrentEpochTimestampInSeconds() - lastUpdatedEpoch) > (60*60*24*7)) {
-    // console.log("invalid cache, over one week old, resetting localStorage and identifiers.")
-  } else {}
-  let cioConfig = { siteID, region, autoPageTracking };
-  if (!storedState) cioSetConfig({...cioConfig});
+  if (storedState) {
+    siteID = storedState.siteID;
+    region = storedState.region;
+  }
+  let cioConfig;
+  if (siteID && region) {
+    cioConfig = { siteID, region };
+  } else {
+    cioConfig = CIO_DEFAULTS;
+  }
   return cioConfig;
 }
 
-export const CIO_JS_SDK_CONFIG = cioGetConfig();
-
-
-function cioSetConfig({ siteID, region, autoPageTracking }) {
+function setCioConfig({ siteID, region }) {
   window.localStorage.setItem(
-    CIO_JS_SDK_LOCALSTORAGE_NAMESPACE,
-    JSON.stringify({ siteID, region, autoPageTracking, lastUpdated: getCurrentEpochTimestampInSeconds() })
+    CIO_LOCALSTORAGE_NAMESPACE,
+    JSON.stringify({ siteID, region })
   );
 }
 
-export function cioResetConfig(){
-  cioSetConfig(CIO_JS_SDK_DEFAULTS)
+export function updateCioConfig({ siteID, region }) {
+  let { siteID: previousSiteId, region: previousRegion } = getCioConfig();
+  if (!previousSiteId && previousRegion) {
+    setCioConfig({ siteID, region });
+  } else if (siteID != previousSiteId || region != previousRegion) {
+    setCioConfig({ siteID, region });
+  } else console.log("Same values provided, No changes made to CIO config");
+  console.log({ siteID, region });
 }
 
-/**
- * Updates CIO Web SDK Config
- * @param {string} siteID _default: `""`_ @param {string} region _default: `"regionUS"`_ @param {string} autoPageTracking _default: `"true"`_
- */
-export function updateCioWebSDKConfig({ siteID=CIO_JS_SDK_DEFAULTS.siteID, region=CIO_JS_SDK_DEFAULTS.region, autoPageTracking=CIO_JS_SDK_DEFAULTS.autoPageTracking }) {
-  let newConfig = { siteID, region, autoPageTracking, lastUpdated: getCurrentEpochTimestampInSeconds() };
-  let currentConfig = cioGetConfig();
-  let changesToConfig = false;
-  for (let key in newConfig) {
-    if (newConfig[key] != currentConfig[key]) {
-      changesToConfig = true;
-      console.log(`Updating ${key}`);
-    }
-  }
-  if (changesToConfig) {
-    cioSetConfig({...newConfig});
-    console.log({...newConfig});
-  }
-}
+export const TSE_CIO_CONFIG = getCioConfig();
 
 function getCookie(c_name) {
   const allCookies = document.cookie;
@@ -88,101 +57,63 @@ function getCookie(c_name) {
   return "";
 }
 
-export function cioGetAnonymousID() {
-  var ajs = getCookie('ajs_anonymous_id');
-  if (ajs && ajs !== '') {
-    // Segment can have quotes in their cookie value, so we'll need to decode
-    // and parse that.
-    return parseString(ajs);
-  }  
+export function getAnonymousID() {
   return getCookie(window._cio.cookieNamespace + "anonid");
 }
 
-function parseString(id) {
+export function getIdentifier() {
   try {
-    return JSON.parse(decodeURIComponent(id));
-  } catch (error) {
-    return id;
-  }
-}
-
-
-export function cioResetIdentifier() {
-  let allowedRejectionReason = "_cio has not yet been added to the window";
-  try {
-    if (window._cio && !Array.isArray(window._cio)) {
-      window._cio.reset();
-      window.localStorage.removeItem("gist.web.userToken");
-      window.location.reload();
-    } else throw "_cio has not yet been added to the window";
+    if (!window._cio) {
+      throw "_cio has not yet been added to the window";
+    }
+    return window._cio._findCustomer();
   } catch (err) {
-    if (err != allowedRejectionReason) console.error(err);
+    console.error(err);
+    return "";
   }
 }
-export async function cioGetIdentifier() {
+
+export async function retrieveIdentifier() {
+  let intervalAttempt = 0;
   return new Promise((resolve,reject)=>{
-    let identifier, anonymousIdentifier;
-    try {
-      if (window._cio) {
-        [identifier, anonymousIdentifier] = (
-          [window._cio._findCustomer(), cioGetAnonymousID()]
-          );
-        resolve({identifier,anonymousIdentifier});
-      } else throw "_cio is not loaded"
-    } catch (err) {
-      reject(err);
+    if (window.TSE_CURRENT_IDENTIFIER) {
+      resolve(window.TSE_CURRENT_IDENTIFIER);
     }
+    let cioLoadedInterval = setInterval(() => {
+      intervalAttempt++;
+      let identifier = getIdentifier();
+      let anonymousIdentifier = getAnonymousID();
+      if (identifier) {
+        clearInterval(cioLoadedInterval);
+        window.TSE_CURRENT_IDENTIFIER = {identifier}
+        resolve({identifier});
+      } else if (anonymousIdentifier) {
+        clearInterval(cioLoadedInterval);
+        window.TSE_CURRENT_IDENTIFIER = {anonymousIdentifier}
+        resolve({anonymousIdentifier});
+      }
+      // If not found, continue attempting every ~10ms for about 1 second
+      if (intervalAttempt > 100) {
+        console.warn("no identifier found");
+        clearInterval(cioLoadedInterval);
+      }
+    }, 10);
   })
 }
 
-function retrieveIdentifier() {
-  try {
-    let {userID} = cdpGetIdentifier();
-    if (userID) window._cio.identify({id:userID})
-  } catch (error) {
-    
-  }
-  return new Promise(async (resolve,reject)=>{
-    let cioID = await cioGetIdentifier().then(res=>res).catch(err=>{reject(err)});
-    if (cioID?.identifier != undefined || cioID?.anonymousIdentifier  != undefined) {
-      resolve(cioID)
-    }
-    else {
-      let intervalAttempt = 0;
-      let cioLoadedInterval = setInterval(async function searchingForIdentifier() {
-        intervalAttempt++;
-        cioID = await cioGetIdentifier().then(res=>res).catch(err=>{return undefined});
-        if ( cioID?.identifier || cioID?.anonymousIdentifier ) {
-          clearInterval(cioLoadedInterval);
-          resolve(cioID);
-        }
-        // If not found, continue attempting every ~100ms for about 1 second
-        if (intervalAttempt > 10) {
-          clearInterval(cioLoadedInterval);
-          reject("no cio identifier found");
-        }
-      }, 100);
-    }
-  })
-}
-
-
-
-export async function cioShowIdentifierElements() {
-  return new Promise(async function fetchingIdentifier(resolve,reject){
-    retrieveIdentifier()
-      .then(id=>{
-        resolve(window.playground._helpers.propogateIdentifier(id))
-      })
-      .catch(function idNotFound(err){reject(err)});
-  }) 
-}
-
-export async function cioIdentify({id,...args}){
-  try {
-    window._cio.identify({id,...args});
-    console.log("_cio identify call sent",{id,...args});
-  } catch (err) {
-    console.error(err)
-  }
+export function showIdentifierElements () {
+  Array.from(document.getElementsByClassName("current-identifier"))
+    .forEach(async (element) => {
+      let currentIdentifier = await retrieveIdentifier().then(id=>id);
+      element.innerHTML = (
+        `<div class="p-2">Your current ${currentIdentifier.identifier ? 
+          "<span class=\"font-bold text-purple-500\">identifier</span>" : 
+          "<span class=\"font-bold text-purple-500\">anonymous identifier</span>"} 
+          is: <em class="font-bold text-purple-500">${
+            currentIdentifier.identifier ? 
+            currentIdentifier.identifier : 
+            currentIdentifier.anonymousIdentifier
+          }</em></div>`
+        );
+    });
 }
